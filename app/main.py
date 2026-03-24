3e002a78e36e62f12497870fa3f3190cd97af08e
Astynax03 <149999577+Astynax03@users.noreply.github.com>
2026-03-24 15:07:48 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://192.168.1.100:5173",  
+        "https://skripsi-chatbot-pra-konseling-upnvj-gamma.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
